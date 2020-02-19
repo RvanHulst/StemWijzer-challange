07dcb9147e373a5e0c2178689e16ad60087890bb
RvanHulst <99051006@mydavinci.nl>
2020-02-19 11:51:49 +0100

    matchen werkend zonder css
    matchen is afgemaakt met de samen met eduard  alleen nog css aan toevoegen en kleine dingen veranderen als nodig is

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let titleDOM = document.querySelector(".title");
 let questionDOM = document.querySelector(".question");
 
+let result_container = document.getElementById("result_container");
+let btnContainer = document.querySelector(".w3-display-bottommiddle");
+
 let btnEens = document.querySelector(".btn1");
 let btnGeenVanBeide = document.querySelector(".btn2");
 let btnOnEens = document.querySelector(".btn3");
-let btnResult = document.querySelector(".btnResult");
 let btnSkip = document.querySelector(".btnSkip");
 let btnBack = document.querySelector(".btnBack");
 let countervraag = 1;
@@ -12,7 +14,7 @@ let counter = 0;
 let answers = [];
 
 function updateQuestion(answer) {
-  if (counter < 12) {
+  if (counter < 30) {
     answers.push(answer);
     titleDOM.innerHTML = countervraag + ". " + subjects[counter].title;
     questionDOM.innerHTML = subjects[counter].statement;
@@ -23,26 +25,19 @@ function updateQuestion(answer) {
   console.log(answers);
 }
 
-function start() {
-  updateQuestion("start");
-}
-
 function displayResult() {
-  titleDOM.innerHTML = "End";
-  questionDOM.innerHTML = "Click result voor het resultaat";
-  btnResult.style.display = "initial";
-  btnEens.style.display = "none";
-  btnGeenVanBeide.style.display = "none";
-  btnOnEens.style.display = "none";
-  btnSkip.style.display = "none";
+  titleDOM.innerHTML = "Resultaten";
+  result_container.style.display = "block";
+  questionDOM.style.display = "none";
   btnBack.style.display = "none";
-
-  document.location.href = "Result.html";
+  btnContainer.style.display = "none";
+  resultContent();
 }
 
 function calcAnswer() {
   for (let i = 0; i < answers.length; i++) {
-    for (let p = 0; p < parties.length; p++) {
+    for (let p = 0; p < parties.length - 1; p++) {
+      console.log(i + " " + p);
       if (subjects[i].parties[p].position == answers[i]) {
         if (!parties[p].score) {
           parties[p].score = +1;
@@ -56,51 +51,54 @@ function calcAnswer() {
 
 function resultContent() {
   calcAnswer();
-
-  var container = document.getElementById("result_container");
-
-  for (let i = 0; i < answers.length; i++) {
+  for (let i = 0; i < parties.length; i++) {
     var p = document.createElement("p");
-    p.innerHTML = parties[i].score;
-    container.appendChild(p);
+    if (parties[i].score) {
+      p.innerHTML = parties[i].name + " " + parties[i].score;
+    } else {
+      p.innerHTML = parties[i].name + " 0";
+    }
+    result_container.appendChild(p);
   }
 }
 
-btnEens.addEventListener("click", () => {
-  if (counter < 12) {
-    counter++;
-    countervraag++;
-    updateQuestion("pro");
-  }
-});
-btnGeenVanBeide.addEventListener("click", () => {
-  if (counter < 12) {
-    counter++;
-    countervraag++;
-    updateQuestion("none");
-  }
-});
-btnOnEens.addEventListener("click", () => {
-  if (counter < 12) {
-    counter++;
-    countervraag++;
-    updateQuestion("contra");
-  }
-});
-btnSkip.addEventListener("click", () => {
-  if (counter < 12) {
-    counter++;
-    countervraag++;
-    updateQuestion("skip");
-  }
-});
-btnBack.addEventListener("click", () => {
-  if (counter > 0) {
-    answers.pop();
-    counter--;
-    countervraag--;
-    titleDOM.innerHTML = countervraag + ". " + subjects[counter].title;
-    questionDOM.innerHTML = subjects[counter].statement;
-    console.log(answers);
-  }
-});
+function initButtons() {
+  btnEens.addEventListener("click", () => {
+    if (counter < 30) {
+      counter++;
+      countervraag++;
+      updateQuestion("pro");
+    }
+  });
+  btnGeenVanBeide.addEventListener("click", () => {
+    if (counter < 30) {
+      counter++;
+      countervraag++;
+      updateQuestion("none");
+    }
+  });
+  btnOnEens.addEventListener("click", () => {
+    if (counter < 30) {
+      counter++;
+      countervraag++;
+      updateQuestion("contra");
+    }
+  });
+  btnSkip.addEventListener("click", () => {
+    if (counter < 30) {
+      counter++;
+      countervraag++;
+      updateQuestion("skip");
+    }
+  });
+  btnBack.addEventListener("click", () => {
+    if (counter > 0) {
+      answers.pop();
+      counter--;
+      countervraag--;
+      titleDOM.innerHTML = countervraag + ". " + subjects[counter].title;
+      questionDOM.innerHTML = subjects[counter].statement;
+      console.log(answers);
+    }
+  });
+}
