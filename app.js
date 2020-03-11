6222b238fe140a2820ec4e5eea9d069a887fd129
RvanHulst <99051006@mydavinci.nl>
2020-03-11 16:25:16 +0100

    bug fixed

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ let result_container = document.getElementById("result_container");
 let null_container = document.getElementById("null_container");
 let btnContainer = document.querySelector(".w3-display-bottommiddle");
 
+let allesBtn = document.getElementById("allesBtn");
+let sclrBtn = document.getElementById("sclrBtn");
+let grootBtn = document.getElementById("grootBtn");
+let partyKeuzen = document.getElementById("partyKeuzen");
+const CHKBOX = document.getElementById("chkBox");
+
 let answers = [];
 let countervraag = 1;
 let counter = 0;
@@ -57,63 +63,99 @@ let counter = 0;
     }
   });
 })();
+//Filter buttons
+allesBtn.addEventListener("click", () => {
+  resultContent("all");
+});
+
+sclrBtn.addEventListener("click", () => {
+  resultContent("sec");
+});
+grootBtn.addEventListener("click", () => {
+  resultContent("groot");
+});
 
 function updateQuestion(answer) {
-  console.log(counter);
-  answers.push(answer);
+  if (CHKBOX.checked) answers.push({ opinion: answer, checked: true });
+  else answers.push({ opinion: answer, checked: false });
+
+  console.log(answers);
+
   titleDOM.innerHTML = countervraag + ". " + subjects[counter].title;
   questionDOM.innerHTML = subjects[counter].statement;
   counter++;
 
   if (counter == subjects.length) {
-    alert();
     displayResult();
-    calcAnswer();
-    sortAnswer();
   }
 }
 
 function displayResult() {
   titleDOM.innerHTML = "Resultaten";
-  result_container.style.display = "block";
   questionDOM.style.display = "none";
   btnBack.style.display = "none";
   btnContainer.style.display = "none";
   chkBox.style.display = "none";
   chkTEXT.style.display = "none";
-  resultContent();
+  partyKeuzen.style.display = "block";
 }
 
-function resultContent() {
+function resultContent(keuzen) {
+  partyKeuzen.innerHTML = "";
   calcAnswer();
   parties.sort((a, b) => a.score - b.score);
 
   for (let i = 0; i < parties.length; i++) {
-    var p1 = document.createElement("p");
-    if (parties[i].score) {
-      p1.innerHTML =
-        parties[i].name +
-        " " +
-        Math.floor((100 / subjects.length) * parties[i].score) +
-        "%";
-      result_container.prepend(p1);
-    } else {
-      p1.innerHTML = parties[i].name + " 0%";
-      null_container.appendChild(p1);
+    if (keuzen == "all") {
+      var p1 = document.createElement("p");
+      if (parties[i].score) {
+        p1.innerHTML = parties[i].name + " " + parties[i].score;
+        result_container.prepend(p1);
+      } else {
+        p1.innerHTML = parties[i].name + " 0";
+        null_container.appendChild(p1);
+      }
+    } else if (keuzen == "sec") {
+      if (parties[i].secular == true) {
+        var p1 = document.createElement("p");
+        if (parties[i].score) {
+          p1.innerHTML = parties[i].name + " " + parties[i].score;
+          result_container.prepend(p1);
+        } else {
+          p1.innerHTML = parties[i].name + " 0";
+          null_container.appendChild(p1);
+        }
+      }
+    } else if (keuzen == "groot") {
+      if (parties[i].size >= 10) {
+        var p1 = document.createElement("p");
+        if (parties[i].score) {
+          p1.innerHTML = parties[i].name + " " + parties[i].score;
+          result_container.prepend(p1);
+        } else {
+          p1.innerHTML = parties[i].name + " 0";
+          null_container.appendChild(p1);
+        }
+      }
     }
   }
 }
 
-//calcutates the answer
+//calcutates the answer\
 function calcAnswer() {
   for (let i = 0; i < answers.length; i++) {
     for (let p = 0; p < parties.length - 1; p++) {
-      console.log(i + " " + p);
-      if (subjects[i].parties[p].position == answers[i]) {
-        if (!parties[p].score) {
-          parties[p].score = +1;
-        } else {
-          parties[p].score = parties[p].score + 1;
+      if (subjects[i].parties[p].position == answers[i].opinion) {
+        for (let part = 0; part < parties.length; part++) {
+          if (subjects[i].parties[p].name == parties[part].name) {
+            if (answers[i]["checked"] == true) {
+              if (!parties[part].score) parties[part].score = 2;
+              else parties[part].score = parties[part].score + 2;
+            } else {
+              if (!parties[part].score) parties[part].score = 1;
+              else parties[part].score = parties[part].score + 1;
+            }
+          }
         }
       }
     }
